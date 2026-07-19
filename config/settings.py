7820f7bd602b4b5b4da46427c76d0139a8c1c778
yassineastati <yassineastati522@gmail.com>
2026-07-19 18:17:57 +0100

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,9 @@ INSTALLED_APPS = [
     "renewals",
 ]
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware", "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware", "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware", "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
